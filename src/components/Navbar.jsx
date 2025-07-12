4c17d52250cfda68c1f5be718fc3cf7461be2ff4
Joaquín Mesa <108308291+joacomesa22@users.noreply.github.com>
2025-07-12 14:15:03 -0300

    Move navigation links to the right side

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,28 +8,26 @@ const Navbar = () => {
         <div className="max-w-[90px] md:max-w-[140px]">
           <a href="#">
             <img src={logo} alt="logo" />
-          </a>
-        </div>
-        <nav>
-          <ul className="flex gap-5 justify-center items-center text-sm">
-            <li className="w-[60px] text-center">
-              <a href="#" className="hover:font-bold">
-                Features
-              </a>
-            </li>
-            <li className="w-[50px] text-center">
-              <a href="#" className="hover:font-bold">
-                Teams
-              </a>
-            </li>
-            <li className="w-[50px] text-center">
-              <a href="#" className="hover:font-bold">
-                Sign In
-              </a>
-            </li>
-          </ul>
-        </nav>
-        <div>
+        <div className="flex items-center gap-6">
+          <nav>
+            <ul className="flex gap-5 justify-center items-center text-sm">
+              <li className="w-[60px] text-center">
+                <a href="#" className="hover:font-bold">
+                  Features
+                </a>
+              </li>
+              <li className="w-[50px] text-center">
+                <a href="#" className="hover:font-bold">
+                  Teams
+                </a>
+              </li>
+              <li className="w-[50px] text-center">
+                <a href="#" className="hover:font-bold">
+                  Sign In
+                </a>
+              </li>
+            </ul>
+          </nav>
         </div>
       </div>
     </header>
