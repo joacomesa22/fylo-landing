53bc206d5f4b79a918e4da54b068b0673b9dc86c
Joaquín Mesa <108308291+joacomesa22@users.noreply.github.com>
2025-07-12 14:15:30 -0300

    Fix syntax errors in Navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,6 +8,8 @@ const Navbar = () => {
         <div className="max-w-[90px] md:max-w-[140px]">
           <a href="#">
             <img src={logo} alt="logo" />
+          </a>
+        </div>
         <div className="flex items-center gap-6">
           <nav>
             <ul className="flex gap-5 justify-center items-center text-sm">
@@ -35,6 +37,3 @@ const Navbar = () => {
 };
 
 export default Navbar;
-
-  )
-}
