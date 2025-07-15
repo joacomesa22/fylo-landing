8ca1ec192e1c91e84bba27de886798ea0f468ced
Joaquín Mesa <108308291+joacomesa22@users.noreply.github.com>
2025-07-15 20:27:50 -0300

    Updated FeatureCard.jsx

--- src/components/FeatureCard.jsx
+++ src/components/FeatureCard.jsx
@@ -5,7 +5,7 @@ const FeatureCard = ({ img, title, text }) => {
     <div className="flex flex-col items-center gap-6 p-6">
       <img src={img} alt={title} className="max-w-[90px]" />
       <div className="flex flex-col items-center text-center gap-3">
-        <h4 className="font-bold text-lg md:text-xl">{title}</h4>
+        <h4 className=ljfhdjkfhd"font-bold text-lg md:text-xl">{title}</h4>
         <p className="text-sm max-w-[300px]">{text}</p>
       </div>
     </div>
