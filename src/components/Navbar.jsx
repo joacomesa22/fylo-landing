08a990fc229fb44edd24d2fdf5e100f6b8886689
Joaquín Mesa <108308291+joacomesa22@users.noreply.github.com>
2025-07-16 17:46:05 -0300

    Add Contact Us button and center navigation

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,7 +10,10 @@ const Navbar = () => {
             <img src={logo} alt="logo" />
           </a>
         </div>
-        <nav className="flex-1 flex justify-end">
+        <button className="px-4 py-2 bg-gradient-to-r from-[var(--cyan)] to-[var(--blue)] rounded-full font-bold text-sm hover:brightness-110 duration-200">
+          Contact Us
+        </button>
+        <nav className="absolute left-1/2 transform -translate-x-1/2">
           <ul className="flex gap-5 justify-center items-center text-sm">
             <li>
               <a href="#" className="hover:font-bold">
