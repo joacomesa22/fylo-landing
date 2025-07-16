626bf364c2da0453f567edc05fe49247b4a0883d
Joaquín Mesa <108308291+joacomesa22@users.noreply.github.com>
2025-07-16 15:26:11 -0300

    Remove Contact Us button and move nav links right

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = () => {
             <img src={logo} alt="logo" />
           </a>
         </div>
-        <nav className="flex-1 flex justify-center">
+        <nav className="flex-1 flex justify-end">
           <ul className="flex gap-5 justify-center items-center text-sm">
             <li>
               <a href="#" className="hover:font-bold">
@@ -29,11 +29,6 @@ const Navbar = () => {
             </li>
           </ul>
         </nav>
-        <div>
-          <button className="px-6 py-2 bg-gradient-to-r from-[var(--cyan)] to-[var(--blue)] rounded-full font-bold text-sm hover:brightness-110 duration-200">
-            Contact Us
-          </button>
-        </div>
       </div>
     </header>
   );
